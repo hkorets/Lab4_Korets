5fe671dfb9c42519ed0916908e580888fb03d616
Khrystya Korets <hkorets0411@gmail.com>
2024-10-03 12:52:24 +0300

    Delete MoreOOP directory

--- MoreOOP/src/main/java/lotr/kickstrategy/HobbitKick.java
+++
@@ -1,9 +0,0 @@
-package lotr.kickstrategy;
-import lotr.Character;
-
-public class HobbitKick implements KickStrategy {
-    @Override
-    public void kick(Character whoKick, Character whoIsKicked) {
-        System.out.println("😢😢😢");
-    }
-}
--- MoreOOP/src/main/java/lotr/kickstrategy/KickStrategy.java
+++
@@ -1,7 +0,0 @@
-package lotr.kickstrategy;
-
-import lotr.Character;
-
-public interface KickStrategy {
-    public void kick(Character whoKick, Character whoIsKicked);
-}
